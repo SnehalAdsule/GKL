19aeee7dacb97d7b3d34799c6831e4809e5feb29
Snehal Adsule <root@spectre-vm01.jf.intel.com>
2020-05-19 12:42:15 -0700

    add logger and clean up

--- src/main/java/com/intel/gkl/pairhmm/IntelPairHmmFpga.java
+++ src/main/java/com/intel/gkl/pairhmm/IntelPairHmmFpga.java
@@ -1,14 +1,19 @@
 package com.intel.gkl.pairhmm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Provides a native PairHMM implementation accelerated using Intel FPGAs
  */
 public final class IntelPairHmmFpga extends IntelPairHmm {
     private static final String NATIVE_LIBRARY_NAME = "gkl_pairhmm_fpga";
+    private final static Logger logger = LogManager.getLogger(IntelPairHmmFpga.class);
 
     public IntelPairHmmFpga() {
-	     //   boolean isLoaded = setNativeLibraryName(NATIVE_LIBRARY_NAME);
+	     // setNativeLibraryName(NATIVE_LIBRARY_NAME);
         	useFpga = true;
+   	    	logger.warn("PairHMM does not support FPGA Implementation. Please use AVX PairHMM");
 
     }
 }
